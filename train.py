f5aec033719084cd56839a439cabd95e880d9ace
Adrian Zimmermann <zadrian@web.de>
2020-11-08 00:02:44 +0100

    changes to training, got a working argmax result

--- train.py
+++ train.py
@@ -8,9 +8,11 @@ import torch.optim as optim
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-def train(dataset: UNetDataset, n_epochs=1):
-    dataloader = DataLoader(dataset, batch_size=1)
+def train(dataset: UNetDataset, n_epochs=1, batch_size=1, cuda=False):
+    dataloader = DataLoader(dataset, batch_size=batch_size)
     model = UNet(size=dataset.item_size, in_channels=dataset.image_channels, classes=dataset.classes, depth=5)
+    if cuda:
+        model.cuda()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
 
@@ -20,33 +22,43 @@ def train(dataset: UNetDataset, n_epochs=1):
     ax2.imshow(torch.argmax(dataset[0][1], dim=0))
     plt.show()
 
+    plt.ion()
+    progress_fig, progress_axes = plt.subplots(1, dataset[0][1].shape[0] + 1)
+    plt.show()
+    plt.pause(.001)
+
     for e in range(0, n_epochs):
 
         running_loss = 0.0
         output = None
         for batch_index, item in enumerate(dataloader):
             image, mask = item
+            if cuda:
+                image = image.cuda()
+                mask = mask.cuda()
             
             optimizer.zero_grad()
 
             predicted_mask = model(image)
-            loss = criterion(predicted_mask.reshape(predicted_mask.shape[0], predicted_mask.shape[1], -1), torch.argmax(mask, dim=1).reshape(mask.shape[0], -1))
+            loss = criterion(predicted_mask.cpu().reshape(predicted_mask.shape[0], predicted_mask.shape[1], -1), torch.argmax(mask.cpu(), dim=1).reshape(mask.shape[0], -1))
             loss.backward()
             optimizer.step()
 
             running_loss = loss.item()
             
-            plt.ion()
-            fig, axes = plt.subplots(1, predicted_mask.shape[1])
+            cpu_predicted_mask = predicted_mask.cpu().detach()
+            
             for i in range(0, predicted_mask.shape[1]):
-                axes[i].imshow(predicted_mask.detach()[0][i])
-            plt.show()
-            plt.pause(.001)
+                progress_axes[i].imshow(cpu_predicted_mask[0][i])
+            progress_axes[len(progress_axes) - 1].imshow(torch.argmax(cpu_predicted_mask[0], dim=0))
+            progress_fig.canvas.draw()
+            progress_fig.canvas.flush_events()
+            progress_fig.show()
 
-            output = predicted_mask.detach()
+            #output = predicted_mask.cpu()
 
             print('running_loss', running_loss)
-            print('max value', output.max())
+            #print('max value', output.max())
         #plt.imshow(torch.argmax(output[0], dim=0))
         #plt.imshow(torch.argmax(nn.functional.softmax(output[0], dim=0), dim=0))
         #plt.show()
